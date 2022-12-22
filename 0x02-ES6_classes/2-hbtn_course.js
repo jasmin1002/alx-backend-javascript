0949949d3d6f254d28734492ce04a309fae4f497
Jasmin1002 <jasmineolofin@gmail.com>
2022-12-22 10:31:48 +0100

    feat: Define and implement HbnCourse class

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,46 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== "string") throw TypeError("Name must be a string");
+    if (typeof length !== "number" || isNaN(length))
+      throw TypeError("Length must be a number");
+    if (!Array.isArray(students))
+      throw TypeError("Students must be an array of string");
+
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof newName !== "string") {
+      throw TypeError("Name must be a string");
+    }
+    this._name = newName;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(newLength) {
+    if (typeof newLength !== "number" || isNaN(newLength)) {
+      throw TypeError("Length must be a number");
+    }
+    this._length = newLength;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(newStudents) {
+    if (!Array.isArray(newStudents)) {
+      throw TypeError("Students must be an array of string");
+    }
+    this._students = newStudents;
+  }
+}
