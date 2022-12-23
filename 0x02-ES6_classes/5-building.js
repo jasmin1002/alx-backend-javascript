cdb0ae54422da2225b6f69443429e74f5b4266d3
Jasmin1002 <jasmineolofin@gmail.com>
2022-12-23 05:41:34 +0100

    feat: Define and implement Building class

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,14 @@
+export default class Building {
+  constructor(sqft) {
+    if (typeof sqft !== "number" || isNaN(sqft))
+      throw TypeError("Square feet must a number");
+    if (this.constructor !== Building && this.evacuationWarningMessage !== "function")
+      throw Error(
+        "Class extending Building must override evacuationWarningMessage"
+      );
+    this._sqft = sqft;
+  }
+  get sqft() {
+    return this._sqft;
+  }
+}
