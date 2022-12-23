fec15d4ca77e1dd525e5643b58a53ed94fc1872d
Jasmin1002 <jasmineolofin@gmail.com>
2022-12-23 03:41:29 +0100

    feat: Define and implement Pricing class

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,43 @@
+import Currency from "./3-currency.js";
+export default class Pricing {
+  constructor(amount, currency) {
+    if (typeof amount !== "number" || isNaN(amount))
+      throw TypeError("Amount must be a number");
+    if (typeof currency !== "object" && typeof currency.name !== "string")
+      throw TypeError("Not a currency");
+
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  set amount(newAmount) {
+    if (typeof newAmount !== "number" || isNaN(newAmount))
+      throw TypeError("Amount must be a number");
+    this._amount = newAmount;
+  }
+  get amount() {
+    return this._amount;
+  }
+
+  set currency(nCurrency) {
+    if (typeof nCurrency !== "object" && typeof nCurrency.name !== "string")
+      throw TypeError("Not a currency");
+    this._currency = nCurrency;
+  }
+
+  get currency() {
+    return this._currency.displayFullCurrency();
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== "number" || isNaN(amount))
+      throw TypeError("Amount must be a number");
+    if (typeof conversionRate !== "number" || isNaN(conversionRate))
+      throw TypeError("conversion rate must a number");
+    return amount * conversionRate;
+  }
+}
