e870ba5b69cf683fe3ceb6e87696b37f7c29319a
Jasmin1002 <jasmineolofin@gmail.com>
2022-12-26 16:36:51 +0100

    feat: Implement SkyHighBuilding extends Building

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,8 +1,17 @@
-import Building from "./5-building.js";
+import Building from './5-building.js';
 
 export default class SkyHighBuilding extends Building {
-    constructor(sqft, floors) {
-        super();
-        
-    }
+  constructor (sqft, floors) {
+    super(sqft);
+    if (typeof floors !== 'number') throw TypeError('Floor must be a number.');
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
 }
