4a56a05570fee8e084cebd401449fe361ac4ad67
Jasmin1002 <jasmineolofin@gmail.com>
2022-12-26 16:01:43 +0100

    fix: Verify the abstract method and use s-quote

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,16 +1,19 @@
 export default class Building {
   constructor(sqft) {
-    if (typeof sqft !== "number" || isNaN(sqft))
-      throw TypeError("Square feet must a number");
+    if (typeof sqft !== 'number' || Number.isNaN(sqft)) {
+      throw TypeError('Square feet must a number');
+    }
     if (
       this.constructor !== Building &&
-      this.evacuationWarningMessage !== "function"
-    )
+      typeof this.evacuationWarningMessage !== 'function'
+    ) {
       throw Error(
-        "Class extending Building must override evacuationWarningMessage"
+        'Class extending Building must override evacuationWarningMessage'
       );
+    }
     this._sqft = sqft;
   }
+
   get sqft() {
     return this._sqft;
   }
