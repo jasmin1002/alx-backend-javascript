bad9f8bd846d887a5c4a6e901a05fe40605068d8
Jasmin1002 <jasmineolofin@gmail.com>
2022-12-26 15:22:17 +0100

    fix: Remove dquote and append commas for eslint

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,31 +1,36 @@
-import Currency from "./3-currency.js";
+import Currency from './3-currency.js'
 export default class Pricing {
   constructor(amount, currency) {
-    if (typeof amount !== "number" || isNaN(amount))
-      throw TypeError("Amount must be a number");
-    if (!(currency instanceof Currency))
-      throw TypeError("Not a currency");
+    if (typeof amount !== 'number' || Number.isNaN(amount)) {
+      throw TypeError('Amount must be a number');
+    }
+    if (!(currency instanceof Currency)) {
+      throw TypeError('Not a currency');
+    }
 
     this._amount = amount;
     this._currency = currency;
   }
 
   set amount(newAmount) {
-    if (typeof newAmount !== "number" || isNaN(newAmount))
-      throw TypeError("Amount must be a number");
+    if (typeof newAmount !== 'number' || Number.isNaN(newAmount)) {
+      throw TypeError('Amount must be a number');
+    }
     this._amount = newAmount;
   }
+
   get amount() {
     return this._amount;
   }
 
   set currency(nCurrency) {
-    if (!(nCurrency instanceof Currency))
-      throw TypeError("Not a currency");
+    if (!(nCurrency instanceof Currency)) {
+      throw TypeError('Not a currency');
+    }
     this._currency = nCurrency;
   }
 
-  get currency() {
+  get currency () {
     return this._currency;
   }
 
@@ -34,10 +39,12 @@ export default class Pricing {
   }
 
   static convertPrice(amount, conversionRate) {
-    if (typeof amount !== "number" || isNaN(amount))
-      throw TypeError("Amount must be a number");
-    if (typeof conversionRate !== "number" || isNaN(conversionRate))
-      throw TypeError("conversion rate must a number");
+    if (typeof amount !== 'number' || Number.isNaN(amount)) {
+      throw TypeError('Amount must be a number');
+    }
+    if (typeof conversionRate !== 'number' || Number.isNaN(conversionRate)) {
+      throw TypeError('conversion rate must a number');
+    }
     return amount * conversionRate;
   }
 }
