9f9423d8c588680f30525a1c6ed9615931912b53
Jasmin1002 <jasmineolofin@gmail.com>
2022-12-27 05:30:25 +0100

    fix: Define variable iterator with const

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (let item of array) {
+  for (const item of array) {
     array.push(appendString + item);
-    // var value = array[idx];
-    // array[idx] = appendString + value;
   }
 
   return array;
