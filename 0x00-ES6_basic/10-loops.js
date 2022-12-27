edf4b5d6796d4a4a080c0d45f312e50b4da4848c
Jasmin1002 <jasmineolofin@gmail.com>
2022-12-27 06:28:13 +0100

    fix: Use variable declaration let instead const

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,9 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   for (const item of array) {
-    // array.push(appendString + item);
-    const idx = array.indexOf(item);
+    let idx = array.indexOf(item);
     array[idx] = appendString + item;
-    // console.log(array.indexOf(idx));
   }
 
   return array;
