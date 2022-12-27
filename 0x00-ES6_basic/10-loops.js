b0c3732957b58aa885b9cc92863829cf92a4a17b
Jasmin1002 <jasmineolofin@gmail.com>
2022-12-27 06:21:19 +0100

    refactor: Get index of element by indexOf()

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
   for (const item of array) {
-    array.push(appendString + item);
+    // array.push(appendString + item);
+    const idx = array.indexOf(item);
+    array[idx] = appendString + item;
+    // console.log(array.indexOf(idx));
   }
 
   return array;
