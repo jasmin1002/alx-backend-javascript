935b780da59442b91e4c8e1846b81eb0c32c431a
Jasmin1002 <jasmineolofin@gmail.com>
2022-12-28 22:17:40 +0100

    fix: Apply eslint-disable for successful compile

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,6 +1,7 @@
 export default function getFullResponseFromApi(success) {
   // promise code goes here
   return new Promise((resolve, reject) => {
+    /* eslint-disable */
     success
       ? resolve({ status: 200, body: 'success' })
       : reject(Error('The fake API is not working currently'));
