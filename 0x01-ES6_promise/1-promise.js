3dc7576e1bebc79a27ec3b158b07da1bdea73f41
Jasmin1002 <jasmineolofin@gmail.com>
2022-12-28 23:08:36 +0100

    refactor: re-structure code further

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -2,7 +2,10 @@ export default function getFullResponseFromApi(success) {
   // promise code goes here
   return new Promise((resolve, reject) => {
     /* eslint-disable */
-    if (success) resolve({ status: 200, body: 'success' });
-    else reject(Error('The fake API is not working currently'));
+    if (success) {
+      resolve({ status: 200, body: 'success' })
+    } else {
+      reject(Error('The fake API is not working currently'))
+    };
   });
 }
