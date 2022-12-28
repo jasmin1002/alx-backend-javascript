0bb9034091d6cd8c144395356cd4a52a7a996dd9
Jasmin1002 <jasmineolofin@gmail.com>
2022-12-28 10:56:00 +0100

    feat: Implement function tranform obj to string

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,8 @@
+export default function iterateThroughObject(reportWithIterator) {
+    const formatString = [];
+
+    for (const item of reportWithIterator) {
+        formatString.push(item);
+    }
+    return formatString.join(' | ');
+}
