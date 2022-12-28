4bcade4fe2d5d60d7d26dd0a9a2dd75a108c6c18
Jasmin1002 <jasmineolofin@gmail.com>
2022-12-28 09:28:52 +0100

    feat: Demonstrate custom iterable and iterator

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,20 @@
+export default function createIteratorObject(report) {
+  const members = [];
+  for (const employees of Object.values(report.allEmployees)) {
+    for (const staff of employees) {
+      members.push(staff);
+    }
+  }
+  let idx = 0;
+  return {
+    [Symbol.iterator]() {
+      return {
+        next() {
+          return idx < members.length
+            ? { done: false, value: members[idx++] }
+            : { done: true };
+        },
+      };
+    },
+  };
+}
