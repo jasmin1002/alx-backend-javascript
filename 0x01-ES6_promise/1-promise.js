9767664379cba9681322b50d40524b1d9d39bc1f
Jasmin1002 <jasmineolofin@gmail.com>
2022-12-29 11:49:59 +0100

    refactor: Revert code to early code structure

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,12 +1,8 @@
 export default function getFullResponseFromApi(success) {
-  // promise code goes here
-  const promise = new Promise((resolve, reject) => {
+  return promise = new Promise((resolve, reject) => {
     /* eslint-disable */
-    if (success) {
-      resolve({ status: 200, body: 'Success' })
-    } else {
-      reject(Error('The fake API is not working currently'))
-    };
+    success
+      ? resolve({ status: 200, body: 'Success' })
+      : reject(Error('The fake API is not working currently'));
   });
-  return promise;
 }
