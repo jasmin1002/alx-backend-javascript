4613555f446215d4a0418912779058f877d68ec5
Jasmin1002 <jasmineolofin@gmail.com>
2022-12-29 14:31:38 +0100

    feat: Collectively resolve promises

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,16 @@
+import { uploadPhoto, createUser } from "./utils.js";
+
+export default function handleProfileSignup() {
+  // code goes here
+  const photo = uploadPhoto();
+  const user = createUser();
+  const promises = [photo, user];
+
+  Promise.all(promises)
+    .then((responses) => {
+      console.log(
+        `${responses[0].body} ${responses[1].firstName} ${responses[1].lastName}`
+      );
+    })
+    .catch(() => console.log("Signup system offline"));
+}
