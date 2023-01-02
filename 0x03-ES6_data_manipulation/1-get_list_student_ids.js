c0cd70b103a0a2b3c5d31ff2cee363464c15529c
Jasmin1002 <jasmineolofin@gmail.com>
2023-01-02 23:03:22 +0100

    fix: Add space before parenthesis for if condition

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,6 @@
 export default function getListStudentIds(students) {
   // returns empty array if argument is other than array
-  if(!Array.isArray(students)) return [];
+  if (!Array.isArray(students)) return [];
   const studentIds = students.map((student) => {
     return student.id;
   });
