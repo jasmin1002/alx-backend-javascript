577476f427108f45fb781dd6ae6688c708e32f0c
Jasmin1002 <jasmineolofin@gmail.com>
2023-01-02 23:09:39 +0100

    fix: Remove non-descriptive comment

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,7 +1,6 @@
 export default function getStudentsByLocation(students, city) {
-  // Implementation
   if (!Array.isArray(students)) return [];
   return students.filter((student) => {
     if (student.location === city) return student;
-  })
+  });
 }
