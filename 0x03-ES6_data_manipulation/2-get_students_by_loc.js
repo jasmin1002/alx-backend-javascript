16ca514c0f0827830f71239a81b710816ca2b7f1
Jasmin1002 <jasmineolofin@gmail.com>
2023-01-02 23:17:52 +0100

    fix: Remove parenthesis around arrow func param

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,6 +1,6 @@
 export default function getStudentsByLocation(students, city) {
   if (!Array.isArray(students)) return [];
-  return students.filter((student) => {
+  return students.filter(student => {
     if (student.location === city) return student;
   });
 }
