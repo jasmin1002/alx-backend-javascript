058c250e608baf67018d2f51555314e085493ceb
Jasmin1002 <jasmineolofin@gmail.com>
2023-01-02 12:57:40 +0100

    feat: Implement function returns list of id

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,8 @@
+export default function getListStudentIds(students) {
+  // returns empty array if argument is other than array
+  if(!Array.isArray(students)) return [];
+  const studentIds = students.map((student) => {
+    return student.id;
+  });
+  return studentIds;
+}
