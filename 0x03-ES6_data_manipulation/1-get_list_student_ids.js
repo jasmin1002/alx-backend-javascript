8e2bc49cea87005b20cec8c6063a37abba26c7b0
Jasmin1002 <jasmineolofin@gmail.com>
2023-01-02 23:33:05 +0100

    refactor: Re-structure code

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,8 +1,5 @@
 export default function getListStudentIds(students) {
   // returns empty array if argument is other than array
   if (!Array.isArray(students)) return [];
-  const studentIds = students.map((student) => {
-    return student.id;
-  });
-  return studentIds;
+  return students.map((student) => student.id);
 }
