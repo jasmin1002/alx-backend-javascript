2ec863a2f0da5a379644c7bd9a2b9253cbafaea5
Jasmin1002 <jasmineolofin@gmail.com>
2023-01-02 22:44:30 +0100

    fix: Append dangling comma to list of items

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -2,6 +2,6 @@ export default function getListStudents() {
   return [
     { id: 1, name: 'Guillaume', location: 'San Francisco' },
     { id: 2, name: 'James', location: 'Columbia' },
-    { id: 5, name: 'Serena', location: 'San Francisco' }
+    { id: 5, name: 'Serena', location: 'San Francisco' },
   ];
 }
