5b3819781c893688756247a6d4cd90d131cde2a3
Jasmin1002 <jasmineolofin@gmail.com>
2023-01-02 23:28:22 +0100

    fix: refactor the callback func to return truthy

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,6 +1,4 @@
 export default function getStudentsByLocation(students, city) {
   if (!Array.isArray(students)) return [];
-  return students.filter(student => {
-    if (student.location === city) return student;
-  });
+  return students.filter(student => student.location === city);
 }
