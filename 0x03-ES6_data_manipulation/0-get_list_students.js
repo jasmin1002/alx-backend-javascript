fcba2927629df71fd55111aacc144edb8bb5c87c
Jasmin1002 <jasmineolofin@gmail.com>
2023-01-02 12:30:19 +0100

    feat: Implement function returns list of objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+export default function getListStudents() {
+  return [
+    { name: 'Guillaume', id: 1, location: 'San Francisco' },
+    { name: 'James', id: 2, location: 'Columbia' },
+    { name: 'Serena', id: 5, location: 'San Francisco' }
+  ];
+}
