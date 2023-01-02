9b23d11041d5fb7d6f35189420d2bbd81f503328
Jasmin1002 <jasmineolofin@gmail.com>
2023-01-02 23:37:14 +0100

    fix: Remove the variable declaration

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,5 +1,4 @@
 export default function getStudentIdsSum(list) {
   if (!Array.isArray(list)) return -1;
-  let sum = list.reduce((acc, cur) => acc + cur.id, 0);
-  return sum;
+  return list.reduce((acc, cur) => acc + cur.id, 0);
 }
