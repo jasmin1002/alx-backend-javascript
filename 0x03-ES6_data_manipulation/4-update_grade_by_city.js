ea87c119d62af63fd76809f614ef1e43e18f9db5
Jasmin1002 <jasmineolofin@gmail.com>
2023-01-03 01:27:16 +0100

    refactor: Re-structure code implementation

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,16 +1,15 @@
 function updateStudentGradeByCity(items, city, updates) {
   // Implementation
   if (!Array.isArray(items)) return [];
-  const students = items.filter((item) => item.location === city);
-  return students.map((student) => {
-    student.grade = 'N/A';
-    for (const { studentId, grade } of updates) {
-      if (student.id === studentId) {
-        student.grade = grade;
+  return items
+    .filter((item) => item.location === city)
+    .map((student) => {
+      student.grade = "N/A";
+      for (const { studentId, grade } of updates) {
+        if (student.id === studentId) student.grade = grade;
       }
-    }
-    return student;
-  });
+      return student;
+    });
 }
 
 export default updateStudentGradeByCity;
