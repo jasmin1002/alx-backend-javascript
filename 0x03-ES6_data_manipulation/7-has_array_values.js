5f804863661aa4fcf0df589c131b4c6554633d33
Jasmin1002 <jasmineolofin@gmail.com>
2023-01-03 15:02:27 +0100

    refactor: change the code structure

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,8 +1,15 @@
+// function hasValuesFromArray(set, array) {
+//   // Implementation
+//   if (!Array.isArray(array)) return -1;
+//   const arrayFromSet = Array.from(set);
+//   const assert = array.every((element) => arrayFromSet.includes(element));
+//   return assert;
+// }
+
 function hasValuesFromArray(set, array) {
-  // Implementation
-  if (!Array.isArray(array)) return -1;
-  const arrayFromSet = Array.from(set);
-  const assert = array.every((element) => arrayFromSet.includes(element));
-  return assert;
+  for (const entry of array) {
+    if (!set.has(entry)) return false;
+  }
+  return true;
 }
 export default hasValuesFromArray;
